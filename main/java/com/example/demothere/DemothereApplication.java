d91c34b80737ad56c189d6824623b0f500c1089f
1822158775 <1822158775@qq.com>
2021-05-21 10:01:14 +0800

    测试IDEA整合GIT

---
+++ main/java/com/example/demothere/DemothereApplication.java
@@ -0,0 +1,13 @@
+package com.example.demothere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemothereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemothereApplication.class, args);
+    }
+
+}
---
+++ test/java/com/example/demothere/DemothereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demothere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemothereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
